98260c5af3d1be74b983e47e98af737fc5a4a512
Wamashudu <Wamashudu.sengani@sovtech.com>
2023-05-02 10:13:12 +0200

    deployment build

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./schema";
 import resolvers from "./champions/resolvers";
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
